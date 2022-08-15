e6e64f3e74c959c31955ae70c53d191c18a32c11
Han Dong <h58dong@uwaterloo.ca>
2022-08-15 03:19:23 -0400

    Sidebar Finished

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,49 @@
+import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
-const Sidebar = ()=>(<>Hello from Side</>)
+import LogoS from '../../assets/images/Heiseliuding.jpg'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from'@fortawesome/react-fontawesome'
+import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+const Sidebar = ()=>(
+<div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img className="sublogo"src={LogoSubtitle} alt='slobodan' />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active"to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" 
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/hanpeng-dong-05989b208/"
+            >
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" 
+                    rel="noreferrer"
+                    href="https://github.com/nanayonn"
+            >
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+</div>
+
+
+
+)
 
 export default Sidebar
