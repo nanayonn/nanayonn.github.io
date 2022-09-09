5868fe7e2e0d0f3bd91a833a34e0d65c64663af4
Han Dong <h58dong@uwaterloo.ca>
2022-09-08 23:24:46 -0400

    cube

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -42,19 +42,19 @@ return(
                     <FontAwesomeIcon icon={faCuttlefish} color="#DD0031" />
                 </div>
                 <div className='face2'>
-                    <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+                    <FontAwesomeIcon icon={faHtml5} color="#F06529" />
                 </div>
                 <div className='face3'>
-                    <FontAwesomeIcon icon={faReact} color="#DD0031" />
+                    <FontAwesomeIcon icon={faReact} color="#28A4D9" />
                 </div>
                 <div className='face4'>
-                    <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+                    <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
                 </div>
                 <div className='face5'>
-                    <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
                 </div>
                 <div className='face6'>
-                    <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                    <FontAwesomeIcon icon={faAngular} color="#EC4" />
                 </div>
             </div>
         </div>
